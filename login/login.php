e049735c6eee4fe645cbe1725937f9c7b8b77a9b
Papitha <41532754+PapithaDharmalingam@users.noreply.github.com>
2019-06-30 20:56:06 +0530

    Update login.php

--- login/login.php
+++ login/login.php
@@ -98,5 +98,5 @@ echo 'alert("INVALID CREDENTIALS")';
     <!-- JS -->
     <script src="vendor/jquery/jquery.min.js"></script>
     <script src="js/main.js"></script>
-</body><!-- This templates was made by Colorlib (https://colorlib.com) -->
+</body>
 </html>
